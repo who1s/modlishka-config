c6e2107036585c1629a7e703d7bfcd2b18d5ff70
Juho Jauhiainen <juho.jauhiainen@nixu.com>
2020-07-22 15:01:43 +0300

    Commit

--- start_modlishka.sh
+++ start_modlishka.sh
@@ -5,7 +5,7 @@ docker build -t phishing-demo-mfa .
 # Generate few example id's
 for i in {1..5}
 do
-	echo "https://localhost/?id="$i
+	echo "https://phishing.fi/?id="$i
 done
 
 docker run -p80:80 -p443:443 -it --rm phishing-demo-mfa Modlishka -config /tmp/modlishka.json
